107a951b62149af826c32090b14dff9d2b3d2bdf
imarinho-dev <imarinho.dev@gmail.com>
2020-06-26 09:58:37 -0300

    Updates

---
+++ precache-manifest.776e6208c00dab9e2476f04b3bf414e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1291c35fcc99910faffdc53e56a091e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "86e40e37afc6a6552f00",
+    "url": "/monsters-rolodex/static/css/main.812bf556.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e40e37afc6a6552f00",
+    "url": "/monsters-rolodex/static/js/main.4fe4c79c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
